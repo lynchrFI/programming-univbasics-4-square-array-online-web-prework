a55acdda2470df3c8e89a402aa8e65af99a13f4f
Ryan Lynch <lynchrbusiness@gmail.com>
2019-09-28 05:48:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
 array.each_with_object([]) { |i,arr| arr << i ** 2 }
 end
-my_arr = [1, 2]
-p square_array(my_arr) #=> [1, 4]
+square_array = [1, 2,3]
+p square_array(array)
 
 
